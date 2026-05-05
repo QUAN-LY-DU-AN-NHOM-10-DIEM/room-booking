5e1a70c20bd21e8de4245f2d2a85b5bffbe2bfc3
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2026-05-05 07:21:58 +0700

    Revert "update BookingModal"
    This reverts commit 4451c5981042b5a02e0aadd49a4d017313e063ad.

--- web/src/components/BookingModal.js
+++ web/src/components/BookingModal.js
@@ -38,17 +38,17 @@ const BookingModal = props => {
       <h3 className="modal__title">Booking Details</h3>
       {booking && (
         <div className="modal__boday">
-          <p className="modal__paragraph"><strong>Title: </strong>{booking['title']}</p>
+          <p className="modal__paragraph"><strong>Title: </strong>{props.selectedBooking['title']}</p>
           <p className="modal__paragraph">
             <strong>Name: </strong>
-            {booking.user ? `${booking.user.firstName} ${booking.user.lastName}` : 'N/A'}
+            {props.selectedBooking.user ? `${props.selectedBooking.user.firstName} ${props.selectedBooking.user.lastName}` : 'N/A'}
           </p>
           <p className="modal__paragraph">
             <strong>Email: </strong>
-            {booking.user ? booking.user.email : 'N/A'}
+            {props.selectedBooking.user ? props.selectedBooking.user.email : 'N/A'}
           </p>
-          <p className="modal__paragraph"><strong>Room: </strong>{findRoomInfo(booking.roomId, props.roomData).name}{', Level '}
-          {findRoomInfo(booking.roomId, props.roomData).floor}</p>
+          <p className="modal__paragraph"><strong>Room: </strong>{findRoomInfo(props.selectedBooking.roomId, props.roomData).name}{', Level '}
+          {findRoomInfo(props.selectedBooking.roomId, props.roomData).floor}</p>
           <p className="modal__paragraph"><strong>Time: </strong>{`${momentTimezone
               .tz(booking['bookingStart'], 'Asia/Ho_Chi_Minh')
             .format('h.mma')} to ${momentTimezone
