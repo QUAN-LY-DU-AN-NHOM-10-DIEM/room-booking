6777d2c4ad149f6d761187c3efead4bc68753012
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2026-05-05 08:23:09 +0700

    update Room Status  style

--- web/src/components/Key.js
+++ web/src/components/Key.js
@@ -4,6 +4,10 @@ const Key = props => (
   <div className="sidebar__box--key key">
     <h3 className="header__heading header__heading--sidebar">Note</h3>
     <div className="key__group">
+      <span className="key__square key__square--available"></span>
+      <p className="key__description">Available</p>
+    </div>
+    <div className="key__group">
       <span className="key__square key__square--pending"></span>
       <p className="key__description">Pending</p>
     </div>
